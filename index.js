94c6b1c57f4c90b8fe34af48ae58306ad0fb8738
Al Tarasenko <alxtaras@icloud.com>
2025-07-13 02:30:08 +0300

    test post and new get

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ require('dotenv').config();
 // Импортируем Express
 const express = require('express');
 const app = express();
+app.use(express.json());
 // Берём порт из .env, или используем 5000 по умолчанию
 const PORT = process.env.PORT || 5000;
+
 // Маршрут по умолчанию
 app.get('/', (req, res) => {
-    res.send('Привет! Сервер работает 👋');
+    res.send('Сервер работает 👋');
+  });
+// post запрос
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.send(`Привет, ${name}!`);
+});
+
+app.get('/hello', (req, res) => {
+    res.send(`Привет`);
   });
 // Запуск сервера
 app.listen(PORT, () => {
-    console.log(`🚀 Сервер запущен: http://localhost:${PORT}`);
+    console.log(`🚀Сервер запущен: http://localhost:${PORT}`);
   });
